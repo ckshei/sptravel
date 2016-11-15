4ca76c17ed795ba2d0e24fe527d84a0f75367b1b
Margaret <eutopia@gmail.com>
2016-11-15 14:57:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Travel Roulette</h2>
+          <h3> Made with awesomeness </h3>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
