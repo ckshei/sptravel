284f941c751ba56a6616b0c57c39a5b7999ed2f2
ckshei <cks856@gmail.com>
2016-11-15 16:07:58 -0500

    set up tripOptions component

---
+++ src/components/sample.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Fetch from 'react-fetch'
+
+class TripOptions extends Component {
+  constructor() {
+    super(); 
+  
+    this.state = {
+      trips: [] 
+    };
+    
+  }
+
+  componentWillMount() {
+    fetch('')
+}
+  render() {
+    return(
+    <div>
+    </div>
+  ) 
+  }  
+}
