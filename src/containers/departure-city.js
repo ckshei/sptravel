5fc3a14a52924a0eb596826e08c48ca0b687b5e5
ckshei <cks856@gmail.com>
2016-11-18 11:00:10 -0500

    adding action

--- src/containers/departure-city.js
+++ src/containers/departure-city.js
@@ -8,6 +8,7 @@ class DepartureCity extends Component {
     super(props);
     this.state = { term: '' };
     this.onInputChange = this.onInputChange.bind(this);
+    this.renderCities = this.renderCities.bind(this)
   }
 
   onInputChange(ev) {
@@ -19,19 +20,34 @@ class DepartureCity extends Component {
     ev.preventDefault();
   }
 
+  renderCities() {
+    if (this.state.cities) {
+      'render cities'
+    } else {
+      'no cities to be rendered' 
+    }
+  }
+
   render() {
     return (
     <form onSubmit={this.onFormSubmit}>
       Departing From:
       <input type='text' placeholder="Departure City" value={this.state.term} onChange={this.onInputChange} />
       <button type="submit"> Confirm </button>
+      {this.renderCities()}
     </form>
   ); 
   }  
+}
+
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  } 
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchCities }, dispatch);
 }
 
-export default connect(null, mapDispatchToProps)(DepartureCity);
+export default connect(mapStateToProps, mapDispatchToProps)(DepartureCity);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import TripsReducer from './reducer_trips';
 import ViewReducer from './reducer_view'
+import CitiesReducer from './reducer_cities'
 
 const rootReducer = combineReducers ({
   trips: TripsReducer,
-  view: ViewReducer
+  view: ViewReducer,
+  cities: CitiesReducer
 });
 
 export default rootReducer;
