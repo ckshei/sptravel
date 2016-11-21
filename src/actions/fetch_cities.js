7a6a67a36e00b6879dff025ffdb1a47a7819bc19
Margaret <eutopia@gmail.com>
2016-11-21 11:23:03 -0500

    working on session

--- src/actions/fetch_cities.js
+++ src/actions/fetch_cities.js
@@ -2,6 +2,8 @@ import fetchJsonp from 'fetch-jsonp';
 
 const API_KEY = 'sp228916872664231778662139964184';
 const CITY_URL = `http://partners.api.skyscanner.net/apiservices/xd/autosuggest/v1.0/US/USD/en-US?query=`;
+const SESSION = 'http://business.skyscanner.net/apiservices/pricing/v1.0/?apikey=prtl6749387986743898559646983194'
+
 export const FETCH_CITIES = 'FETCH_CITIES';
 
 
@@ -10,6 +12,10 @@ export function fetchCities(city) {
 
   const request = fetchJsonp(url)
     .then(res => res.json())
+
+    const session = fetch(SESSION, {
+    	method: 'POST'
+    })
 
   console.log('Request:', request);
 
