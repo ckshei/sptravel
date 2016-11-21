7edbb95479f62378d96e9a49790f2f8fd298f7e6
ckshei <cks856@gmail.com>
2016-11-21 11:55:39 -0500

    creating actions

--- src/actions/set_query.js
+++ src/actions/set_query.js
@@ -1,29 +1,33 @@
 export const SET_CITY = "SET_CITY";
 export const SET_DEPARTURE = "SET_DEPARTURE";
 export const SET_RETURN  = "SET_RETURN";
-export connst SET_BUDGET = "SET_BUDGET";
+export const SET_BUDGET = "SET_BUDGET";
 
 export function setCity(city) {
   return {
-      type: SET_CITY
+      type: SET_CITY,
+      payload: city
   };
 }
 
-export function setDeparture() {
+export function setDeparture(departure) {
   return {
-    type: SET_DEPARTURE
+    type: SET_DEPARTURE,
+    payload: departure
   };
 }
 
-export function setReturn() {
+export function setReturn(return) {
   return {
-    type: SET_RETURN
+    type: SET_RETURN,
+    payload: return
   };
 }
 
-export function setBudget() {
+export function setBudget(budget) {
   return {
-  type: SET_BUDGET
+  type: SET_BUDGET,
+  payload: budget
   };
 }
 
