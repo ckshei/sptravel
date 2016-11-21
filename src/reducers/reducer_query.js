719ed8ca41cbe9706342b360f9d37c2b95c21b46
Margaret <eutopia@gmail.com>
2016-11-21 14:38:41 -0500

    reducer

--- src/reducers/reducer_query.js
+++ src/reducers/reducer_query.js
@@ -1,1 +1,16 @@
 import { SET_RETURN, SET_CITY, SET_BUDGET, SET_DEPARTURE } from '../actions/set_query'
+
+export default function(state = null, action) {		
+  switch(action.type) {		
+    case SET_RETURN:		
+      return action.payload		
+    case SET_CITY:		
+      return action.payload		
+    case SET_BUDGET:		
+      return action.payload		
+    case SET_DEPARTURE:		
+      return action.payload		
+    default:		
+      return state		
+  }		
+}
