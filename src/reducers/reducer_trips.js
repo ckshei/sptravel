1d31112a9a09ceb0c04eed5cd6bff419684cdc69
ckshei <cks856@gmail.com>
2016-12-05 14:55:02 -0500

    removed trips trips bug

--- src/reducers/reducer_trips.js
+++ src/reducers/reducer_trips.js
@@ -3,7 +3,7 @@ import { FETCH_TRIPS } from '../actions/fetch_trips.js'
 export default function(state = null, action) {
   switch(action.type) {
   case FETCH_TRIPS:
-    return {trips: action.payload}
+    return: action.payload
   default:
     return state
   }
